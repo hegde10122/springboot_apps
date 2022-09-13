e586e7d62b29eb488c2e10e723ef852481ce51b0
hegde10122 <hegde10122@gmail.com>
2022-09-13 07:52:19 +0530

    update

--- security/demoapp/src/main/java/com/hegde/security/config/ProjectSecurityConfig.java
+++ security/demoapp/src/main/java/com/hegde/security/config/ProjectSecurityConfig.java
@@ -1,8 +1,40 @@
 package com.hegde.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
 }
+
+/**
+ * @Configuration
+ *                public class ProjectSecurityConfig {
+ * 
+ * @Bean
+ *       public SecurityFilterChain filterChain(HttpSecurity http) throws
+ *       Exception {
+ *       http
+ *       .authorizeHttpRequests((authz) -> authz
+ *       .anyRequest().authenticated()
+ *       )
+ *       .httpBasic(withDefaults());
+ *       return http.build();
+ *       }
+ * 
+ *       }
+ */
