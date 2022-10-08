ac58920c0cdd1bdeebf12e0b57f267a72ba878c3
hegde10122 <hegde10122@gmail.com>
2022-10-08 14:54:29 +0530

    new app
    new app

---
+++ springbootlearning/src/test/java/com/hegde/springbootlearning/SpringbootlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hegde.springbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
