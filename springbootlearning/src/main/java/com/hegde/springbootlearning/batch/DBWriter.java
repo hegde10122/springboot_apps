3ba894473ab1cbde0bead93375a6cfba7ff2a6c5
hegde10122 <hegde10122@gmail.com>
2022-10-11 16:45:25 +0530

    update
    update

---
+++ springbootlearning/src/main/java/com/hegde/springbootlearning/batch/DBWriter.java
@@ -0,0 +1,24 @@
+package com.hegde.springbootlearning.batch;
+
+import com.hegde.springbootlearning.model.User;
+import com.hegde.springbootlearning.repository.UserRepository;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DBWriter implements ItemWriter<User> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void write(List<? extends User> list) throws Exception {
+
+        System.out.println("Data saved for users "+list);
+        userRepository.saveAll(list);
+
+    }
+}
--- springbootlearning/src/main/java/com/hegde/springbootlearning/config/SpringBatchConfig.java
+++ springbootlearning/src/main/java/com/hegde/springbootlearning/config/SpringBatchConfig.java
@@ -10,8 +10,15 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 
 @Configuration
@@ -31,5 +38,32 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public FlatFileItemReader<User> fileItemReader(@Value())
+    public FlatFileItemReader<User> fileItemReader(@Value("${input}") Resource resource){
+
+        FlatFileItemReader<User> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(resource);
+        flatFileItemReader.setName("CSV Reader");
+        flatFileItemReader.setLinesToSkip(1);
+        flatFileItemReader.setLineMapper(lineMapper());
+        return flatFileItemReader;
+    }
+
+    @Bean
+    public LineMapper<User> lineMapper(){
+
+        DefaultLineMapper<User> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames(new String[] {"id","name","dept","salary"});
+
+        BeanWrapperFieldSetMapper<User> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(User.class);
+
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+
+        return defaultLineMapper;
+    }
+
 }
