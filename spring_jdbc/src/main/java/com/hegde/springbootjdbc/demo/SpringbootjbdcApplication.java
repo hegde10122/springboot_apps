da1fe71cd37cfd96df3c8da8d8dc66dfd6e6fa16
hegde10122 <hegde10122@gmail.com>
2022-10-19 17:06:21 +0530

    springboot jdbc
    springboot jdbc

---
+++ spring_jdbc/src/main/java/com/hegde/springbootjdbc/demo/SpringbootjbdcApplication.java
@@ -0,0 +1,13 @@
+package com.hegde.springbootjdbc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjbdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjbdcApplication.class, args);
+	}
+
+}
---
+++ spring_jdbc/src/test/java/com/hegde/springbootjdbc/demo/SpringbootjbdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hegde.springbootjdbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjbdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
