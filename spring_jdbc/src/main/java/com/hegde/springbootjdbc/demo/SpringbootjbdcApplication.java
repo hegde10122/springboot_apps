4f4a92d9d1cdd347a924e54387362b3c257100c5
hegde10122 <hegde10122@gmail.com>
2022-10-22 13:49:51 +0530

    update
    update

--- spring_jdbc/src/main/java/com/hegde/springbootjdbc/demo/SpringbootjbdcApplication.java
+++ spring_jdbc/src/main/java/com/hegde/springbootjdbc/demo/SpringbootjbdcApplication.java
@@ -1,11 +1,13 @@
 package com.hegde.springbootjdbc.demo;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.hegde.springbootjdbc.demo.dao.EmployeeDao;
 import com.hegde.springbootjdbc.demo.json.Employee;
 import com.hegde.springbootjdbc.demo.json.ObjectsMapper;
+import com.hegde.springbootjdbc.demo.json.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +17,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @SpringBootApplication
 public class SpringbootjbdcApplication { // implements CommandLineRunner
@@ -36,7 +37,7 @@ public class SpringbootjbdcApplication { // implements CommandLineRunner
 
 
 	private static final Logger logger = LoggerFactory.getLogger(SpringbootjbdcApplication.class);
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(SpringbootjbdcApplication.class, args);
 	//	readFile();
 		//readJsonWithObjectMapper();
@@ -44,7 +45,173 @@ public class SpringbootjbdcApplication { // implements CommandLineRunner
 
 	//	readJsontreeNode();
 
-		writeJsonJackson(readJsonWithObjectMapper());
+	//	writeJsonJackson(readJsonWithObjectMapper());
+
+		//writeJacksonJsonGenerator();
+		//parseJsonJackson();
+
+		//parseJsonArray();
+		//jsonArrayToListMap();
+		configureSerializer();
+
+	}
+
+	private static void configureSerializer() {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		String jsonString = "{\"name\" : \"Hegde\", \"gender\" : \"Male\",\"address\" : \"Mumbai\" }";
+
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false);
+
+		try {
+			Student student = objectMapper.readValue(jsonString, Student.class);
+			JsonNode jsonNodeRoot = objectMapper.readTree(jsonString);
+			JsonNode jsonNodeYear = jsonNodeRoot.get("address");
+			String address = jsonNodeYear.asText();
+			System.out.println(address);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+		}
+
+
+	public class CustomStudentSerializer extends StdSerializer<Student> {
+
+		protected CustomStudentSerializer() {
+			this( null);
+		}
+		protected CustomStudentSerializer(Class<Student> t) {
+			super(t);
+		}
+
+		@Override
+		public void serialize(Student student, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+			jsonGenerator.writeStartObject();
+			jsonGenerator.writeStringField("student_surname", student.getName());
+			jsonGenerator.writeEndObject();
+		}
+	}
+	private static void jsonArrayToListMap() throws JsonProcessingException {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonStudentArray =
+				"[{\"name\" : \"Hegde\", \"gender\" : \"Male\" }, { \"name\" : \"Kala\", \"gender\" : \"Female\" }]";
+		List<Student> studentList = objectMapper.readValue(jsonStudentArray, new TypeReference<>() {
+		});
+
+		String json = "{ \"color\" : \"Red\", \"type\" : \"BMW\" }";
+
+		Map<String, Object> map
+				= objectMapper.readValue(json, new TypeReference<Map<String,Object>>(){});
+
+		System.out.println(map);
+
+		studentList.forEach(System.out::println);
+
+
+	}
+
+	private static void parseJsonArray() {
+
+		String json = "{ \"name\" : \"Chandan\", \"gender\" : \"Male\" }";
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try{
+			JsonNode jsonNode = objectMapper.readTree(json);
+			String gender = jsonNode.get("gender").asText();
+			System.out.println(gender);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+	private static void parseJsonJackson() {
+
+		String json
+				= "{\"name\":\"hegde\",\"age\":25,\"address\":[\"Kailash\",\"Lily road\"]}";
+		JsonFactory jfactory = new JsonFactory();
+
+		try{
+			JsonParser jParser = jfactory.createParser(json);
+
+			String parsedName = null;
+			Integer parsedAge = null;
+			List<String> addresses = new LinkedList<>();
+
+			while (jParser.nextToken() != JsonToken.END_OBJECT) {
+				String fieldname = jParser.getCurrentName();
+				if ("name".equals(fieldname)) {
+					jParser.nextToken();
+					parsedName = jParser.getText();
+				}
+
+				if ("age".equals(fieldname)) {
+					jParser.nextToken();
+					parsedAge = jParser.getIntValue();
+				}
+
+				if ("address".equals(fieldname)) {
+					jParser.nextToken();
+					while (jParser.nextToken() != JsonToken.END_ARRAY) {
+						addresses.add(jParser.getText());
+					}
+				}
+			}
+			jParser.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+
+	}
+
+	private static void writeJacksonJsonGenerator() {
+
+		/*
+		 {
+	   "name":"hegde",
+  		 "age":25,
+ 	  "address":[
+      "Kailash",
+      "Beautiful Road"
+  		 ]
+		}
+		  */
+
+
+
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		JsonFactory jfactory = new JsonFactory();
+
+		try{
+			JsonGenerator jGenerator = jfactory
+					.createGenerator(stream, JsonEncoding.UTF8);
+
+			jGenerator.writeStartObject();
+			jGenerator.writeStringField("name", "hegde");
+			jGenerator.writeNumberField("age", 25);
+			jGenerator.writeFieldName("address");
+			jGenerator.writeStartArray();
+			jGenerator.writeString("Kailash");
+			jGenerator.writeString("Beautiful Road");
+			jGenerator.writeEndArray();
+			jGenerator.writeEndObject();
+			jGenerator.close();
+
+			String json = new String(stream.toByteArray(), "UTF-8");
+			System.out.println(json);
+		}catch (Exception e){
+
+		}
+
+
 	}
 
 	private static void writeJsonJackson(Employee employee) {
